f6a3aba0734d6158a6e4a6ac979ebdc59603f5c5
Eduardo <trejo.alfredo.es@outlook.com>
2022-12-22 14:13:09 -0600

    Loging API Created

---
+++ CronicaRegNotesRetriver/Google_API_Credential.py
@@ -0,0 +1,42 @@
+#librerias para enviar correo
+import os
+import pickle
+# Gmail API utils
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+# for encoding/decoding messages in base64
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+# for dealing with attachement MIME types
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from mimetypes import guess_type as guess_mime_type
+
+
+class Loging_Google_API():
+    def __init__(self):
+        # Request all access (permission to read/send/receive emails, manage the inbox, and more)
+        self.SCOPES = ['https://mail.google.com/']
+        self.our_email = 'notas.automaticas@gmail.com'
+
+    def gmail_authenticate(self,Credential_Path):
+        creds = None
+        # the file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first time
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
+                creds = pickle.load(token)
+        # if there are no (valid) credentials availablle, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(Credential_Path, self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # save the credentials for the next run
+            with open("token.pickle", "wb") as token:
+                pickle.dump(creds, token)
+        return build('gmail', 'v1', credentials=creds)
---
+++ CronicaRegNotesRetriver/Main.py
@@ -0,0 +1,22 @@
+
+from Google_API_Credential import Loging_Google_API
+from GmailAPI import Mailing
+
+
+## Instances init
+GoogleClientAPI = Loging_Google_API()
+GmailMailling = Mailing("notas.automaticas@gmail.com")
+
+
+
+## Authenticate app and PC to be able to use Google API
+Service = GoogleClientAPI.gmail_authenticate("CronicaRegNotesRetriver/GoogleAPI_Credentials/credentials.json")
+
+
+imeges_attached=["/Users/eduardo/Desktop/RecoleccionNotas_CronicaReg/CronicaRegNotesRetriver/Images/Titular de SEDESU presenta avances en agenda ambiental0.jpg"]
+mail_to= "paginalalo9@gmail.com"
+mail_obj = "Titulo de nota"
+mail_body = " messages automatique de part de lalo !!! Dounia est la plus belle ¡¡¡¡"
+
+
+GmailMailling.send_message(Service, mail_to, mail_obj, mail_body, imeges_attached)
